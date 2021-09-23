c045f8c79f765972dd0986148f630362efcb1579
ipioselli <ines.pioselli@gmail.com>
2021-09-23 19:44:22 -0400

    activity 3

---
+++ Week 3/Activity-2/js/script.js
@@ -0,0 +1,63 @@
+/**
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+
+
+let bg = {
+  r: 0,
+  g: 0,
+  b: 0,
+
+};
+
+let circle1 = {
+  x: 0,
+  y: 250,
+  size: 100,
+  fill: 255,
+  alpha: 200
+
+
+};
+
+let circle2 = {
+  x: 500,
+  y: 250,
+  size: 75,
+  fill: 255,
+  alpha: 200
+
+};
+/**
+Description of setup
+*/
+function setup() {
+  createCanvas(500, 500);
+  noStroke();
+
+}
+
+function draw() {
+background(bg.r, bg.g, bg.b);
+bg.r = bg.r +1;
+
+//circle1
+circle1.x = circle1.x +1;
+circle1.x = constrain(circle1.x, 0, width/2);
+circle1.size = circle1.size +1;
+circle1.size = constrain(circle1.size, 0, width );
+fill(circle1.fill, circle1.alpha);
+ellipse(circle1.x, circle1.y, circle1.size);
+
+//circle2
+circle2.x = circle2.x + -1;
+circle2.x = constrain(circle2.x, width/2, width);
+circle2.size = circle1.size * 0.75;
+fill(circle2.fill, circle2.alpha);
+ellipse(circle2.x, circle2.y, circle2.size);
+}
