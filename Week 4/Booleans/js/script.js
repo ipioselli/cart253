d8aaeb2fa7cf744269abef4a56c79be5ee20b0c1
ipioselli <ines.pioselli@gmail.com>
2021-10-01 19:49:50 -0400

    started learning about loops

--- Week 4/Booleans/js/script.js
+++ Week 4/Booleans/js/script.js
@@ -1,14 +1,45 @@
+//let displayCircle = false;
+
+let caterpillar = {
+  x: 100,
+  y: 250,
+  segmentSize: 50
+}
+
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(500,500);
 }
 
 function draw() {
-  if(mouseIsPressed === true){
-    background(255);
+
+  background(0);
+  noStroke();
+  fill(100,200,100);
+
+  let x = caterpillar.x;
+  let numSegments = 5;
+  let segmentsDrawn = 0;
+
+  while (segmentsDrawn < numSegments) {
+    ellipse(x, caterpillar.y, caterpillar.segmentSize);
+    x = x + 40;
+    segmentsDrawn = segmentsDrawn + 1;
+
   }
-  else{
-    background(0);
+
+  ellipse(caterpillar.x, caterpillar.y, caterpillar.segmentSize);
+  x = x + 40;
+
+
+
+ /** if (mouseIsPressed){
+    displayCircle = true;
+  } //if the mouse is pressed the circle will appear
+
+  if(displayCircle){
+    ellipse(250, 250, 100, 100);
   }
+  */
 
 }
