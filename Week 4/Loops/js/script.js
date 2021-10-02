3d48e618834e921d0db9734434cef17945e07830
ipioselli <ines.pioselli@gmail.com>
2021-10-01 22:37:52 -0400

    A4: Dodging Covid 19

--- Week 4/Loops/js/script.js
+++ Week 4/Loops/js/script.js
@@ -3,20 +3,35 @@ let circle = {
   y: undefined,
   size: 100
 }
-/**
-Description of setup
-*/
+
+let dangerZone = {
+  x: 250,
+  y: 250,
+  size: 150
+};
+
+
 function setup() {
   createCanvas(500, 500);
 
   circle.x = random(0, width);
   circle.y = random(0, height);
 
+  let d = dist(circle.x, circle.y, dangerZone.x, dangerZone.y);
+  while (d < circle.size/2 + dangerZone.size/2) {
+    circle.x = random(0,width);
+    circle.y = random(0,height);
+    d = dist(circle.x, circle.y, dangerZone.x, dangerZone.y);
+  }
 }
-
 
 function draw() {
   background(0);
+
+  //danger zone
+  noFill();
+  stroke(255, 0, 0);
+  ellipse(dangerZone.x, dangerZone.y, dangerZone.size);
 
   fill(255);
   noStroke();
