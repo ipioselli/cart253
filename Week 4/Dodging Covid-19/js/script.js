caf98afe4b9c43c8c25fcb0b8f7fa98f43da26ae
ipioselli <ines.pioselli@gmail.com>
2021-10-01 22:51:46 -0400

    adding and if statement

--- Week 4/Dodging Covid-19/js/script.js
+++ Week 4/Dodging Covid-19/js/script.js
@@ -1,33 +1,44 @@
-/**
-Title of Project
-Author Name
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
-"use strict";
+//Dodging Covid-19
+//Ines Pioselli
 
 
-/**
-Description of preload
-*/
-function preload() {
+let covid19 = {
+  x: 0,
+  y: 250,
+  size: 100,
+  vx:0,
+  vy: 0,
+  speed: 5,
+  fill: {
+    r: 255,
+    g: 0,
+    b: 0
 
+  }
 }
 
-
-/**
-Description of setup
-*/
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  covid19.y = random(0, height);
+  covid19.vx = covid19.speed;
 
 }
 
 
-/**
-Description of draw()
-*/
 function draw() {
+  background(0);
+
+  covid19.x = covid19.x + covid19.vx;
+  covid19.y = covid19.y + covid19.vy;
+
+  if(covid19.x > width){
+    covid19.x = 0;
+    covid19.y = random(0, height);
+  }
+
+  fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
+  ellipse(covid19.x, covid19.y, covid19.size);
 
 }
