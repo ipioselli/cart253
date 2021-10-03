adc461c075efcdf4f2fea4e4a95987a4fc73ebf7
ipioselli <ines.pioselli@gmail.com>
2021-10-02 23:16:07 -0400

    setting up images

--- Exercises/Dodge-em/js/script.js
+++ Exercises/Dodge-em/js/script.js
@@ -1,27 +1,46 @@
-/**
-Title of Project
-Author Name
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
+//Dodge Em
+//Ines Pioselli
 
-"use strict";
+let bg ={
+  r: 41,
+  g: 29,
+  b: 105
+
+}
+
+let problem1 = {
+  x: 0,
+  y: 250,
+  size: 100,
+  vx:0,
+  vy:0,
+  speed: 5,
+  image:undefined
+}
+
+let user = {
+  x: 250,
+  y:250,
+  size: 150,
+  image:undefined
+}
 
 
-/**
-Description of preload
-*/
 function preload() {
+
+  user.image = loadImaeg("assets/images/cryingebichu.png");
+  problem1.image= loadImage("assets/images/knife.png");
 
 }
 
 
-/**
-Description of setup
-*/
-function setup() {
 
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    problem1.y = random(0, height);
+    //problem1.size = random();
+    problem1.vx = problem1.speed;
 }
 
 
@@ -29,5 +48,11 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(bg.r, bg.g, bg.b);
+
+  for (let i = 0; i <1000; i++){
+    let x = random(0, width);
+    let y = random(0, height);
+    point(x, y);
 
 }
