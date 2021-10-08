de9c212985b11304b02ed831dae3c47347d90d4a
ipioselli <ines.pioselli@gmail.com>
2021-10-07 22:06:09 -0400

    changing font

--- Week 5/Parameters/js/script.js
+++ Week 5/Parameters/js/script.js
@@ -17,18 +17,20 @@ function setup() {
 function draw() {
   background(0);
 
-  parallels(100,100);
-  parallels(50, 50);
+  parallels(100,100, 5, 1, 100, 1); //draws 5 lines
+  parallels(50, 50, 10, 2, 20, 10); // draws 10 lines
+  parallels(200,200, 15, 7, 3, 20); // draws 15 lines
+  parallels(312, 257, 20, 0.5, 300, 2); // draws 20 lines
 }
 
 
-  function parallels(x,y) {
-    for (let i = 0; i < 20; i++) {
+  function parallels(x,y, numLines, lineWidth, lineHeight, lineSpacing) {
+    for (let i = 0; i < numLines; i++) {
       noStroke();
       fill(255);
       rectMode(CENTER);
-      rect(x, y, 2, 150);
-      x =  x + 10;
+      rect(x, y, lineWidth, lineHeight);
+      x =  x + lineSpacing;
 
     }
   }
