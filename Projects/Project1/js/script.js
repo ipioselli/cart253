5c45a9d6d3c08ef613081090bbfa81c7c14dc728
ipioselli <ines.pioselli@gmail.com>
2021-10-21 00:09:20 -0400

    adding surprise element

--- Projects/Project1/js/script.js
+++ Projects/Project1/js/script.js
@@ -12,7 +12,7 @@ This is a Bunny Planet Astrology Simulator
 */
 
 "use strict";
-
+let myFont;
 
 let speedStar = 0.1;
 
@@ -39,7 +39,8 @@ let shootingStar = {
   x:0,
   y:0,
   size:400,
-  vx:0,
+  vx:1,
+  vy:1,
   speed:5
 }
 
@@ -57,9 +58,10 @@ let mercury= {
 let venus= {
   x:0,
   y:0,
+  size: 80,
   angle: 0,
-  radius: 50,
-  speed: -0.03502 * speedStar,
+  radius: 200,
+  speed: -0.003,
 
 };
 
@@ -134,12 +136,14 @@ Description of preload
 */
 function preload() {
 bg.image = loadImage("assets/images/bg.png");
+myFont = loadFont('assets/fonts/balloon.otf');
 
 user.image = loadImage("assets/images/user.png");
 
 //display planets
 sun.image = loadImage("assets/images/sun.png");
 mercury.image = loadImage("assets/images/Mercury.png");
+venus.image = loadImage("assets/images/Venus.png");
 }
 
 let state = `title`; //launches the title screen
@@ -166,11 +170,9 @@ shootingStar.vy = random(-shootingStar.speed, shootingStar.speed);
 user.vx = random(-user.speed, user.speed);
 user.vy = random(-user.speed, user.speed);
 
-
-
 }
 
-//function simulation(){}
+
 /**
 Description of draw()
 */
@@ -185,6 +187,9 @@ if(state ===`title`){
 }
 else if(state ===`simulation`){
   simulation();
+}
+else if(state === `dead`){
+  dead();
 }
 
 }
@@ -207,6 +212,7 @@ function title(){
   push();
   image(user.image, 960, 540, user.size*5, user.size*5);
   pop();
+  textFont(myFont);
 
   textSize(40);
   fill(200, 200, 200);
@@ -219,9 +225,17 @@ function simulation(){
   display();
   userInput();
   move();
+  checkShootingStar()
 
 }
 
+function dead(){
+  textFont(myFont);
+
+  textSize(40);
+  fill(200, 200, 200);
+  text(`RIP`, width/2, height/2);
+}
 
 
 
@@ -254,7 +268,7 @@ function venusPlanet(){
   translate(centerX, centerY);
   rotate(venus.angle);
   venus.angle = venus.angle + venus.speed;
-  ellipse(venus.radius, 0, 10, 10);
+  image(venus.image,venus.radius, 0, venus.size, venus.size);
   pop();
 }
 
@@ -325,6 +339,14 @@ function move(){
   shootingStar.x = shootingStar.x + shootingStar.vx;
   shootingStar.y = shootingStar.y + shootingStar.vy;
 
+  if(shootingStar.y < 0 || shootingStar.y >1080){
+    shootingStar.y = 1080;
+  }
+
+  shootingStar.x  = constrain(shootingStar.x , 0, width);
+  shootingStar.y  = constrain(shootingStar.y , 0, width);
+
+
 }
 
 function userInput(){
@@ -350,8 +372,18 @@ function userInput(){
   }
 
 }
+
+function checkShootingStar(){
+  let d = dist(user.x, user.y, shootingStar.x, shootingStar.y);
+  if(d < user.size/2 -100 + shootingStar.size/2 -100){
+    state = `dead`;
+  }
+}
+
+
 function display(){
   image(user.image, user.x, user.y, user.size, user.size);
+  ellipse(shootingStar.x, shootingStar.y, 20, 20);
 }
 
 function keyPressed(){
