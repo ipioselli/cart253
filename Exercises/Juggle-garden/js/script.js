8d0ed86fe9f451a80df1e575981d9c797bce6d36
ipioselli <ines.pioselli@gmail.com>
2021-10-28 12:30:46 -0400

    created 3 new classes

--- Exercises/Juggle-garden/js/script.js
+++ Exercises/Juggle-garden/js/script.js
@@ -1,13 +1,36 @@
 /**
-Title of Project
-Author Name
+Exercise #5
+Juggle-Garden
+Ines Pioselli
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Brief:
+1. Add another form of user control
+2. add a new class and objects
+3. add at least 2 endings
+
 */
 
 "use strict";
 
+let user;
+
+let garden = {
+
+  mushrooms:[],
+  numMushrooms : 15,
+
+  flowers:[],
+  numFlowers: 15,
+
+
+
+  grassColor: {
+    r:132,
+    g:179,
+    b: 107
+  }
+
+};
 
 /**
 Description of preload
@@ -21,6 +44,12 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(windowHeight, windowWidth);
+
+
+  for(let i =0; i < garden.numMushrooms; i++){
+    
+  }
 
 }
 
