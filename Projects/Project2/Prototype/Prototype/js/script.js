8829c89dd87fb145f91857d39ff64b063109871d
ipioselli <ines.pioselli@gmail.com>
2021-11-11 13:24:53 -0500

    P2: updated readme

---
+++ Projects/Project2/Prototype/Prototype/js/script.js
@@ -0,0 +1,33 @@
+/**
+Project 2 Prototype
+Long Leg Fishy Dating Simulator
+Ines Pioselli
+
+
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
