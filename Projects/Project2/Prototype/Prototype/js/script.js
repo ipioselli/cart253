1a1be236032e7b73173ffa84901856cb249389a9
ipioselli <ines.pioselli@gmail.com>
2021-11-11 15:05:48 -0500

    P2: Display Prototype titlescreen

--- Projects/Project2/Prototype/Prototype/js/script.js
+++ Projects/Project2/Prototype/Prototype/js/script.js
@@ -8,11 +8,30 @@ Ines Pioselli
 
 "use strict";
 
+let tutorialButton = {
+  x:350,
+  y:530,
+  size:520,
+}
+
+
+//image for the title state
+let titleScreen = {
+  x:400,
+  y:400,
+  size:800,
+}
+
+
+let state = `title`; // the prototype starts with the title state
+
 
 /**
 Description of preload
 */
 function preload() {
+  titleScreen.image = loadImage("assets/images/titleScreen.png");
+  tutorialButton.image = loadImage("assets/images/tutorialButton.png");
 
 }
 
@@ -21,6 +40,7 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(800, 800);
 
 }
 
@@ -29,5 +49,25 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  if(state === `title`){
+    title();
+  }
+  else if(state === `tutorial`){
+    tutorial();
+  }
+  else if(state === `page01`){
+    page01();
+  }
+
+}
+
+function title(){
+  imageMode(CENTER, CENTER);
+  image(titleScreen.image, titleScreen.x, titleScreen.y, titleScreen.size, titleScreen.size);
+  displayTutorialButton();
+}
+
+function displayTutorialButton(){
+  image(tutorialButton.image, tutorialButton.x, tutorialButton.y, tutorialButton.size, tutorialButton.size);
 
 }
