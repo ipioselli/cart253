76d76a0a14e44bf13b1584d1c0cd4072fce0fa39
ipioselli <ines.pioselli@gmail.com>
2021-11-12 21:42:17 -0500

    EX 6: Started make some noise exercise

--- Acitivties/10.5 Sound Acitivity/js/script.js
+++ Acitivties/10.5 Sound Acitivity/js/script.js
@@ -5,19 +5,45 @@ Ines Pioselli
 
 */
 
+
+
 "use strict";
 
+let balls = [];
 
+//f minor
+let notes = [`F3`, `G3`, `Ab4`, `Bb4`, `C4`, `Db4`, `Eb4`, `F4`];
 function preload() {
 
 }
 
 
 function setup() {
+  createCanvas(600, 600);
+
+  userStartAudio();
 
 }
 
 
 function draw() {
+  background(0);
 
+  for(let i =0; i < balls.length; i++){
+    let ball = balls[i]; // temporary variable
+    ball.move();
+    ball.bounce();
+    ball.display();
+  }
+
+}
+
+function mousePressed(){
+  createBall(mouseX, mouseY);
+}
+
+function createBall(x, y){
+  let note = random(notes);
+  let ball = new Ball(x, y, note);
+  balls.push(ball);
 }
